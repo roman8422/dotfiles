ba3fade4fc86627994999c9cc73f50dc3f69b2f4
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2016-08-06 14:53:08 +0000

    Add vagrant configuration

--- install.sh
+++ install.sh
@@ -1,14 +1,28 @@
 # Make links for vim
 
+TIMESTAMP=$(date +%s)
+
 VIMRC=~/.vimrc
 if [ $(ls $VIMRC 2>/dev/null| wc -l) -ge 1 ]; then
-    mv $VIMRC $VIMRC.old
+    mv $VIMRC{,.$TIMESTAMP}
 fi
 ln -s ~/dotfiles/vimrc $VIMRC
 
-DIR=~/.vim
-if [ $(ls $DIR 2>/dev/null | wc -l) -ge 1 ]; then
-    mv  $DIR $DIR.old
+VIMDIR=~/.vim
+if [ $(ls $VIMDIR 2>/dev/null | wc -l) -ge 1 ]; then
+    mv  $VIMDIR{,.$TIMESTAMP}
 fi
 ln -s ~/dotfiles/vim ~/.vim
 
+VAGRANTDDIR=~/.vagrant.d
+if [ ! -d "$VAGRANTDDIR" ]; then
+    mkdir $VAGRANTDDIR
+fi
+
+VAGRANTFILE=$VAGRANTDDIR/Vagrantfile
+
+if [ -f $VAGRANTFILE ]; then
+   mv $VAGRANTFILE{,.$TIMESTAMP}
+else
+    ln -s ~/dotfiles/vagrant.d/Vagrantfile $VAGRANTFILE
+fi
