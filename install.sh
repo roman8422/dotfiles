f82d9fd61c14d1678665364b9acdd11840255fde
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2016-08-15 11:05:52 +0300

    Change install script conditions and logic

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 TIMESTAMP=$(date +%s)
 
 VIMRC=~/.vimrc
-if [ $(ls $VIMRC 2>/dev/null| wc -l) -ge 1 ]; then
+if [ -a $VIMRC ]; then
     mv $VIMRC{,.$TIMESTAMP}
 fi
 ln -s ~/dotfiles/vimrc $VIMRC
 
 VIMDIR=~/.vim
-if [ $(ls $VIMDIR 2>/dev/null | wc -l) -ge 1 ]; then
+if [ -a $VIMDIR ]; then
     mv  $VIMDIR{,.$TIMESTAMP}
 fi
 ln -s ~/dotfiles/vim ~/.vim
@@ -22,9 +22,8 @@ fi
 VAGRANTFILE=$VAGRANTDDIR/Vagrantfile
 if [ -f $VAGRANTFILE ]; then
    mv $VAGRANTFILE{,.$TIMESTAMP}
-else
-    ln -s ~/dotfiles/vagrant.d/Vagrantfile $VAGRANTFILE
 fi
+ln -s ~/dotfiles/vagrant.d/Vagrantfile $VAGRANTFILE
 
 CFG=~/.ansible.cfg
 if [ -a $CFG ]; then
